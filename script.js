e47b03f345ff2b01ada65e3376a6cae0bbefe8a0
Yusufhoja <usufhozah@gmail.com>
2024-03-15 17:10:27 +0500

    js 10-dars

---
+++ script.js
@@ -0,0 +1,31 @@
+// // decloration
+// azizhoja ();
+// function azizhoja(){
+//     console.log("salom");
+// }
+
+// // expression
+// const myFunction = function(){
+//     console.log("alik");
+// }
+
+// // arrow
+// const lyuboy =()=>{
+//     console.log("qalesan");
+// };
+// function Hukmatilla (name, second){
+//     console.log(`salom ${name} ${second}`);
+// }
+// Hukmatilla("Fayzullo ", "qalesan");
+
+let a = +prompt("birinchi sonni kiriting")
+let b = +prompt("ikkinhci sonni kiriting")
+function c (second) {
+    if(a>b){
+        alert(`Eng katta son: ${a}`);
+    }
+    else{
+        alert(`Eng katta son: ${b}`);
+    }
+}
+c ()
